bc12b23ec97d1310362f40175d8adbd88295c9ef
Mohd Saif <iamg6102@gmail.com>
2025-07-31 21:29:31 +0530

    IDK

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
-
-
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
+        const connectionIns = await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
+        console.log(`MongoDB connected! DB Host: ${connectionIns.connection.host}`);
     } catch (error) {
-        console.log("Error of connection of DB ",error);
-        process.exit(1)
-        
+        console.error("Error connecting to MongoDB:", error.message);
+        process.exit(1);
     }
-}
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
 import mongoose from "mongoose";
-import {DB_NAME} from "./constants";
+import {DB_NAME} from "./constants.js";
 import express from "express";
+import connectDB from "./db/db.js";
+
+
+connectDB();
 
 
 
